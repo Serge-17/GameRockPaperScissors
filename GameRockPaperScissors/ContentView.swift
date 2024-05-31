1ab743e768e0af0a293b9e0c325d98880143120b
Serge-17 <sergei.eliseev17@icloud.com>
2024-05-31 10:08:29 +0300

    Initial Commit

---
+++ GameRockPaperScissors/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GameRockPaperScissors
+//
+//  Created by Serge Eliseev on 31.05.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GameRockPaperScissors/GameRockPaperScissorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  GameRockPaperScissorsApp.swift
+//  GameRockPaperScissors
+//
+//  Created by Serge Eliseev on 31.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct GameRockPaperScissorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
